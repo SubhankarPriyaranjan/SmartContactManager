e8708a30f2c6329944b4d4df95b91425a7e58e34
Subhankar Priyaranjan <subhankar.rout@smaketsolutions.com>
2024-07-12 18:55:17 +0530

    Added Node and The Controller

--- src/main/java/com/scm/controller/HomeController.java
+++ src/main/java/com/scm/controller/HomeController.java
@@ -3,6 +3,9 @@ package com.scm.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.scm.forms.UserForm;
 
 
 
@@ -45,7 +48,18 @@ public class HomeController {
     //register router 
     @RequestMapping("/register")
     public String Register(){
+//         UserForm userForm = new UserForm();
+    
+// model.addAttribute("userform", userForm);
+
         return "register";
     }
     
+    
+    @RequestMapping(value = "/do-register", method = RequestMethod.POST)
+    public String processRegister(){
+        //fetch from data
+
+        return "redirect:/register";
+    }
 }
